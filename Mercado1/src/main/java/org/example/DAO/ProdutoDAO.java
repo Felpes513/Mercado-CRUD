52debae06b92aae95a087eacf2ad7690f66f2565
Felipe Souza Moreira <110871291+Felpes513@users.noreply.github.com>
2024-08-20 15:15:08 -0300

    Add files via upload

---
+++ Mercado1/src/main/java/org/example/DAO/ProdutoDAO.java
@@ -0,0 +1,115 @@
+package org.example.DAO;
+
+import com.mysql.cj.MysqlConnection;
+import org.example.conexao.conexaoMySQL;
+import org.example.Modelo.Produto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+    //CRUD
+
+    //metodo de cadastro de objetos
+
+    public void cadastrarProduto(Produto produto) {
+        String sql = "INSERT INTO produtos (nome, preco, quantidade) VALUES (?, ?, ?)";
+
+        try (Connection conn = conexaoMySQL.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getQuantidade());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar produto:" + e.getMessage());
+        }
+    }
+
+    public List<Produto> listarProduto() {
+        List<Produto> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+
+        try (Connection conn = conexaoMySQL.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()){
+                Produto produto = new Produto(rs.getString("nome"), rs.getDouble("preco"), rs.getInt("quantidade"));
+                produtos.add(produto);
+            }
+        }catch (SQLException e){
+            System.out.println("Erro ao listar produtos:" + e.getMessage());
+        }
+        return produtos;
+    }
+    public void atualizarProduto(Produto produto){
+        String sql = "UPDATE produtos SET nome = ?, preco = ?, quantidade = ? WHERE id = ?";
+
+        try (Connection conn = conexaoMySQL.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getQuantidade());
+            stmt.setInt(4, produto.getId());
+
+            int rowsAffected = stmt.executeUpdate();
+
+            if (rowsAffected > 0){
+                System.out.println("Produto atualizado com sucesso\n");
+            }else {
+                System.out.println("Produto não econtrado\n");
+            }
+        }catch (SQLException e){
+            System.out.println("Erro ao atualizar o produto:\n" + e.getMessage());
+        }
+    }
+    public Produto buscarProdutoPeloId(int id){
+        String sql = "SELECT * FROM produtos where id = ?";
+        Produto produto = null;
+
+        try(Connection conn = conexaoMySQL.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()){
+                produto = new Produto(
+                        rs.getString("nome"),
+                        rs.getDouble("preco"),
+                        rs.getInt("quantidade")
+                );
+                produto.setId(rs.getInt("id"));
+            }
+        }catch (SQLException e){
+            System.out.println("Erro ao buscar o produto:" + e.getMessage());
+        }
+        return produto;
+    }
+
+    public void deletarProduto(int id){
+        String sql = "DELETE FROM produtos WHERE id =?";
+
+        try(Connection conn = conexaoMySQL.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1, id);
+            int rowsAffected = stmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Produto deletado com sucesso!");
+            }else {
+                System.out.println("Produto não encontado");
+            }
+            }catch (SQLException e) {
+            System.out.println("Erro ao deletar produto:" + e.getMessage());
+        }
+    }
+}
