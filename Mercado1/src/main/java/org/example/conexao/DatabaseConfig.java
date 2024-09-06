85225212f6adc435e7de78bca3783671f708cceb
Felipe Souza Moreira <110871291+Felpes513@users.noreply.github.com>
2024-09-06 14:30:02 -0300

    Delete Mercado1 directory

--- Mercado1/src/main/java/org/example/conexao/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package org.example.conexao;
-
-public class DatabaseConfig {
-    public  static final String URL = "jdbc:mysql://localhost:3307/Mercado";
-    public static final String USER = "root";
-    public static final String SENHA = "2210";
-}
